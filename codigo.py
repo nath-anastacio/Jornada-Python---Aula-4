70fc8f203821b75e905dbf899c8605adaeab90e7
Nathalia <nathanastacio@gmail.com>
2024-10-14 18:33:37 -0300

    Definindo passo a passo

---
+++ codigo.py
@@ -0,0 +1,15 @@
+#Tela inicial
+    #Título: HashZapp
+    #Botão: "Iniciar Chat"
+    #Ao clicar no botão: 
+        #Abrir um popup:
+            #Título: Bem vindo(a) ao HashZapp
+            #Caixa de texto: Escreva seu nome
+            #Botão: Entrar no chat
+            #Ao clicar no botão:
+                #Sumir com o título
+                #Carregar o campo de enviar mensagem: "Digite sua mensagem"
+                #Botão "Enviar"
+                #Quando clicar no botão:
+                    #Enviar a mensagem
+                    #Limpar a caixa de mensagem
